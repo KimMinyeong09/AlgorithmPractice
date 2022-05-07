1609d5bb70ae7f6ab81f33105cc6297854c8c06e
minyeong <minyeong9070@naver.com>
2022-05-07 20:46:36 +0900

    [LeetCode]Merge Two Sorted List

---
+++ LinkedList/Merge Two Sorted Lists/MergeList.py
@@ -0,0 +1,11 @@
+![screencapture-leetcode-explore-interview-card-top-interview-questions-easy-93-linked-list-771-2022-05-07-20_40_14](https://user-images.githubusercontent.com/70676475/167252716-ad16baf7-9a62-4ff2-91f9-9ae0d5b567ac.png)
+
+### 문제 주소
+[Merge Two Sorted Lists](https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/771/)
+
+### 접근 방법
+새 node `cur`을 만들어서 `list1`과 `list2`를 차례로 비교해서  
+작은 값을 가지는 node부터 `cur`에 연결
+
+### 시간복잡도
+O(n)
